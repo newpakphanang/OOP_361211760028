d5e8cc9240bd65f0312cc2e74bef972ee04bc79b
new <newpaknang39@gmail.com>
2020-07-17 13:44:50 +0700

    add more code

--- src/LAB1/HelloWorld.java
+++ src/LAB1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World!!!");
+        System.out.println("nontiwat chanthong");
+        System.out.println("Management Information Technology");
+        System.out.println("RUTS");
     }
 
 
