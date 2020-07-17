29ba41e577a92b7bb14ad0882cb8574da1a7170a
newpakphanang <68414682+newpakphanang@users.noreply.github.com>
2020-07-17 13:53:18 +0700

    Update HelloWorld.java

--- src/LAB1/HelloWorld.java
+++ src/LAB1/HelloWorld.java
@@ -7,7 +7,8 @@ public class HelloWorld {
         System.out.println("Hello World!!!");
         System.out.println("nontiwat chanthong");
         System.out.println("Management Information Technology");
-
+  
+        System.out.println("test Pull Request");
     }
 
 
