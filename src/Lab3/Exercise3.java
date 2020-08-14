685330f7ad8c2eecb957167379858006c8493f07
new <newpaknang39@gmail.com>
2020-08-14 16:13:00 +0700

    Exercise3 Lab3

---
+++ src/Lab3/Exercise3.java
@@ -0,0 +1,58 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Enter your score");
+        int s = scanner.nextInt();
+
+        if (s== 1){      //int or char
+            System.out.println("Wow, I like berger at KFC");
+        } else if (s== 2){
+            System.out.println("Your have to spennd 300 THB per person.");
+        }else if (s==3) {
+            System.out.println("I do not like pizza.");
+            System.out.println("Please, Enter score between 1-3.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+}
