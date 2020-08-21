a2f872eea108d4ccf30c957e9a3836ad1fc61b02
new <newpaknang39@gmail.com>
2020-08-21 16:21:57 +0700

    Exercise3 Lab3

---
+++ src/Lab3/Ex_if_else.java
@@ -0,0 +1,23 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Ex_if_else {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter integer 1: ");
+        int x = scanner.nextInt();
+        System.out.println("Enter integer 2: ");
+        int y = scanner.nextInt();
+
+        //find grater number
+        if (x > y) {
+            System.out.println(x + "grater than " + y);
+        } else {
+            System.out.println(x + " less than " + y);
+        }
+        System.out.println( "Good Bye" );
+    }
+    }
---
+++ src/Lab3/Exercise_Lab3_Login.java
@@ -0,0 +1,66 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Exercise_Lab3_Login {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int num;
+        do {System.out.print("Welcome to MT Website ");
+        num = sc.nextInt();
+
+
+        }while (num !)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
